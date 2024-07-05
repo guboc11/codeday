4be807131bd4b2d67fd6e986473bcfa626f48d27
guboc11 <guboc11@gmail.com>
2024-07-06 00:39:35 +0900

    Add api server

--- build/src/app.js
+++ build/src/app.js
@@ -4,10 +4,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+class Post {
+    constructor(title, body, createdAt) {
+        this.title = title;
+        this.body = body;
+        this.createdAt = createdAt;
+    }
+}
+var posts = [];
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
 app.set("port", process.env.PORT || 3000);
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.json({
+        title: "get",
+        body: "body"
+    });
+});
+app.get("/posts", (req, res) => {
+    res.json(posts);
+});
+app.post("/", (req, res) => {
+    const { title, body, createdAt } = req.body;
+    const newPost = new Post(title, body, createdAt);
+    console.log(newPost);
+    posts.push(newPost);
+    res.json(posts);
 });
 app.listen(app.get("port"), () => {
     console.log(app.get("port"), "번에서 대기중");
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,46 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+
+class Post {
+  title : string;
+  body : string;
+  createdAt : Date;
+
+  constructor(title : string, body : string, createdAt : Date) {
+    this.title = title;
+    this.body = body;
+    this.createdAt = createdAt;
+  }
+}
+
+
+var posts : Post[] = [];
+
 
 const app = express();
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    title: "get",
+    body: "body"
+  });
+});
+
+app.get("/posts", (req, res) => {
+  res.json(posts)
+})
+
+app.post("/", (req, res) => {
+  const { title, body, createdAt } = req.body;
+  const newPost : Post = new Post(title, body, createdAt);
+  console.log(newPost)
+  posts.push(newPost)
+  res.json(posts)
 });
 
 app.listen(app.get("port"), () => {
